81416f9ce9cceca7bba13fb1be8016c0555c8096
Safoora Heidari <safoora.heidari@yahoo.com>
2021-10-25 23:59:58 +0330

    tablink is Ok now

--- js/tap30-services.js
+++ js/tap30-services.js
@@ -1,20 +1,20 @@
-function openTab(evt, tabName) {
+function openTab(evt, tabName, linkName) {
     // Declare all variables
-    let i, tabcontent, tablinks;
+    let tabcontent, tablinks;
 
     // Get all elements with class="tab-content" and hide them
     tabcontent = document.getElementsByClassName("tab-content");
-    for (i = 0; i < tabcontent.length; i++) {
+    for (let i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
     // Get all elements with class="tab-links" and remove the class "active"
     tablinks = document.getElementsByClassName("tab-links");
-    for (i = 0; i < tablinks.length; i++) {
+    for (let i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace("active", "");
     }
 
     // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += "active";
+    document.getElementById(tabName).style.display = "flex";
+    document.getElementById(linkName).classList.add("active");
 }

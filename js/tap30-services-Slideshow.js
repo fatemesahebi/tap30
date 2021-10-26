ba6dac1067561d12667761d271d85240a58f95f9
Mahed <mahedyar@gmail.com>
2021-10-26 19:02:53 +0330

    slideshow js merged to services js and slideshow loop has been fixed

--- js/tap30-services-Slideshow.js
+++ js/tap30-services-Slideshow.js
@@ -1,19 +1,19 @@
-let myIndex = 0;
-slideShow();
-
-function slideShow() {
-    let i;
-    let text = document.getElementsByClassName("tab-content");
-    let icon = document.getElementsByClassName("tab-links");
-    for (i = 0; i < text.length; i++) {
-        text[i].style.display = "none";
-        icon[i].classList.remove("active");
-    }
-    myIndex++;
-    if (myIndex > text.length) {
-        myIndex = 1;
-    }
-    text[myIndex - 1].style.display = "flex";
-    icon[myIndex - 1].classList.add("active");
-    setTimeout(slideShow, 2000); // Change image every 2 seconds
-}
+// let myIndex = 0;
+// slideShow();
+//
+// function slideShow() {
+//     let i;
+//     let text = document.getElementsByClassName("tab-content");
+//     let icon = document.getElementsByClassName("tab-links");
+//     for (i = 0; i < text.length; i++) {
+//         text[i].style.display = "none";
+//         icon[i].classList.remove("active");
+//     }
+//     myIndex++;
+//     if (myIndex > text.length) {
+//         myIndex = 1;
+//     }
+//     text[myIndex - 1].style.display = "flex";
+//     icon[myIndex - 1].classList.add("active");
+//     setTimeout(slideShow, 2000); // Change image every 2 seconds
+// }
--- js/tap30-services.js
+++ js/tap30-services.js
@@ -1,6 +1,10 @@
+let stopSlide = 0 ;
+
+
 function openTab(evt, tabName, linkName) {
     // Declare all variables
     let tabcontent, tablinks;
+    stopSlide = 1 ;
 
     // Get all elements with class="tab-content" and hide them
     tabcontent = document.getElementsByClassName("tab-content");
@@ -17,4 +21,26 @@ function openTab(evt, tabName, linkName) {
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabName).style.display = "flex";
     document.getElementById(linkName).classList.add("active");
+}
+
+let myIndex = 0;
+slideShow();
+
+function slideShow() {
+    let i;
+    let text = document.getElementsByClassName("tab-content");
+    let icon = document.getElementsByClassName("tab-links");
+    if (stopSlide === 0) {
+        for (i = 0; i < text.length; i++) {
+            text[i].style.display = "none";
+            icon[i].classList.remove("active");
+        }
+        myIndex++;
+        if (myIndex > text.length) {
+            myIndex = 1;
+        }
+        text[myIndex - 1].style.display = "flex";
+        icon[myIndex - 1].classList.add("active");
+        setTimeout(slideShow, 2000); // Change image every 2 seconds
+    }
 }
